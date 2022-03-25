a19947d7b4a36ddf97ee2bb529d9c48c343fa458
Eduardo <eduardo.baez@mercadolibre.cl>
2022-03-25 13:20:42 -0300

    chg: formatted categories model

--- NotBored/Screens/CategoriesScreen/Model/CategoriesModel.swift
+++ NotBored/Screens/CategoriesScreen/Model/CategoriesModel.swift
@@ -1,5 +1,15 @@
 import Foundation
 
 struct CategoriesModel {
-    let categories = ["education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"].sorted()
+    let categories = [
+        "education",
+        "recreational",
+        "social",
+        "diy",
+        "charity",
+        "cooking",
+        "relaxation",
+        "music",
+        "busywork"
+    ].sorted()
 }

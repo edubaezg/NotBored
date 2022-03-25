e96567786c5e355eeedc16d862eda1677387c92b
Eduardo <eduardo.baez@mercadolibre.cl>
2022-03-25 11:58:13 -0300

    fix: removed unused init for activity model

--- NotBored/Modules/Activity/Model/ActivityModel.swift
+++ NotBored/Modules/Activity/Model/ActivityModel.swift
@@ -1,10 +1,8 @@
 import Foundation
 
 struct ActivityModel: Codable {
-    var activity: String = ""
-    var type: String = ""
-    var participants: Int = 0
-    var price: Double = 0.0
-    
-    init() {}
+    var activity: String
+    var type: String
+    var participants: Int
+    var price: Double
 }
